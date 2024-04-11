e0cba352feb5abe0acc963f53c77cf1b870d7f1e
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-12 01:17:31 +0800

    multiple input

--- request.py
+++ request.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 print('Put some skill that you are not familiar with')
-unfamiliar_skill = input('>')
+unfamiliar_skill = input('>').split(',')
 print(f'Filtering out {unfamiliar_skill}')
 
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
@@ -14,7 +14,7 @@ for job in jobs:
         company_name = job.find('h3', class_='joblist-comp-name').text.replace('  ', '')
         skills = job.find('span', class_='srp-skills').text.replace('  ', '')
         more_info = job.header.h2.a['href']
-        if unfamiliar_skill not in skills:
+        if not any(skill.strip() in skills for skill in unfamiliar_skill):
             print(f"Company Name: {company_name.strip()}")
             print(f"Required Skills: {skills.strip()}")
             print(f"More Info: {more_info}")
